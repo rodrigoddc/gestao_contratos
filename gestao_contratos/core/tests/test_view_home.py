3112f715c1c3c48a649ae516780a326432749ad5
rodrigo <rodrigoddc1@gmail.com>
2021-04-07 18:28:26 -0300

    added flake8

--- gestao_contratos/core/tests/test_view_home.py
+++ gestao_contratos/core/tests/test_view_home.py
@@ -1,14 +1,13 @@
 from django.shortcuts import resolve_url
 from django.test import TestCase
 
-# Create your tests here.
+
 class HomeViewGet(TestCase):
-	def setUp(self) -> None:
-		self.response = self.client.get(resolve_url('core:home'))
+    def setUp(self) -> None:
+        self.response = self.client.get(resolve_url('core:home'))
 
-	def test_view_home_status(self):
-		self.assertTrue(200, self.response.status_code)
+    def test_view_home_status(self):
+        self.assertTrue(200, self.response.status_code)
 
-	def test_view_home_template_used(self):
-		self.assertTemplateUsed(self.response, 'core/home.html')
-
+    def test_view_home_template_used(self):
+        self.assertTemplateUsed(self.response, 'core/home.html')
--- gestao_contratos/core/views.py
+++ gestao_contratos/core/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
+
 def home(request):
-	return render(request, 'core/home.html')
+    return render(request, 'core/home.html')
 
 
 def atleta_cadastrar(request):
-	return render(request, 'core/form_athlete.html')
+    return render(request, 'core/form_athlete.html')

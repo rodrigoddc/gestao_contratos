36c017d2cc59d3de949af40aca402a039227b6ea
rodrigo <rodrigoddc1@gmail.com>
2021-04-07 10:11:59 -0300

    appconfig name

--- gestao_contratos/settings.py
+++ gestao_contratos/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # project apps
-    'gestao_contratos.core',
+    'gestao_contratos.core.apps.SubscriptionsConfig',
 ]
 
 MIDDLEWARE = [

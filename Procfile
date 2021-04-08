d5125353e3396ccea6cd5f60247b316cbaa2ba2c
rodrigo <rodrigoddc1@gmail.com>
2021-04-08 00:18:30 -0300

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gestao_contratos.wsgi --log-file -
---
+++ gestao_contratos/core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from gestao_contratos.core.views import home, atleta_cadastrar
+
+app_name = "core"
+
+urlpatterns = [
+    path("", home, name="home"),
+    path("formulario/", atleta_cadastrar, name="formulario"),
+]

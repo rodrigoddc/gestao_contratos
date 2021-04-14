7747199aea1c3b8ef02b749775a4dcb6b45d6be8
rodrigo <rodrigoddc1@gmail.com>
2021-04-14 11:34:48 -0300

    DONE: Login/logout using django builtins

--- gestao_contratos/core/urls.py
+++ gestao_contratos/core/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from gestao_contratos.core.views import home, atleta_cadastrar, login
+from gestao_contratos.core.views import home, atleta_cadastrar, LoginUser, LogoutUser
 
 app_name = "core"
 
 urlpatterns = [
     path("", home, name="home"),
     path("formulario/", atleta_cadastrar, name="formulario"),
-    path("login/", login, name="login"),
+    path("login/", LoginUser.as_view(), name="login"),
+    path("logout-user/", LogoutUser.as_view(), name="logout"),
 ]
--- gestao_contratos/core/views.py
+++ gestao_contratos/core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 def home(request):
     return render(request, "core/home.html")
@@ -9,5 +11,9 @@ def atleta_cadastrar(request):
     return render(request, "core/form_athlete.html")
 
 
-def login(LoginView):
-    pass
+class LoginUser(LoginView):
+    template_name = 'core/login.html'
+
+
+class LogoutUser(LogoutView):
+    template_name = 'core/home.html'

f274fa3cd59a195ff1e8cddad7b70d58d93112c3
rodrigo <rodrigoddc1@gmail.com>
2021-04-15 08:38:29 -0300

    DONE: added custom titles to admin

--- gestao_contratos/core/admin.py
+++ gestao_contratos/core/admin.py
@@ -1,3 +1,5 @@
-# from django.contrib import admin
+from django.contrib.admin import AdminSite
 
-# Register your models here.
+AdminSite.site_header = 'Gestão de Contratos Atletas'
+AdminSite.site_title = 'Administração da Gestão de contratos'
+AdminSite.index_title = 'Administrativo'
